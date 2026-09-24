22d7fdc3a5740bf5eb5356d78243c5193a2cd271
Colin-Holbrook <70405386+Colin-Holbrook@users.noreply.github.com>
2026-09-24 14:25:56 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
               />
               <p className="mx-auto max-w-[800px] text-xl md:text-2xl text-[#3b3183]">
                  
-               Understand and combat overreliance and parasocial bonding with AI.
+               To understand and resist overtrust and parasocial bonding with AI.
               </p>
             </div>
             <div className="space-x-4 pt-4">
